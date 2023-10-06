42f73799c9792484cae78503a675d30740bf431a
Dev-scott <sadoscott25@gmail.com>
2023-10-06 18:28:17 +0100

    update

--- pages/api/address/index.js
+++ pages/api/address/index.js
@@ -42,6 +42,8 @@ export default async function handler(req, res) {
        const userId = session.user._id
        console.log(userId)
 
+       
+
       getAddresses(req,res , userId);
         // return res.status(200).json({ addresses });
 
